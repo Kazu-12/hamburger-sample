5600bb813459d89ff17bed671d73fd4e77bbaf48
Kazu <relakkumalikehotcake@gmail.com>
2021-05-29 07:04:12 +0900

    html>php

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hamburger|ダミーサイト</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <script src="./js/jquery-3.6.0.min.js"></script>
-    <script src="./js/script.js"></script>
-</head>
-
-<body>
-    <div class="l-contents">
-        <div class="l-contents__left">
-            <header class="l-header">
-                <div class="c-button"><a class="c-button__menu">Menu</a></div>
-                <div class="p-header-title">
-                    <h1>Hamburger</h1>
-                    <div class="p-search">
-                        <div class="p-search__box">
-                            <input class="p-search__txt" type="text" name="search">
-                        </div>
-                        <input class="p-search__button" type="submit" value="検索">
-                    </div>
-                </div>
-            </header>
+<?php get_header(); ?>
         
             <main class="l-main">
                 <div class="p-main-visual p-main-visual--front-page">
@@ -77,60 +51,6 @@
             </main>
         </div>
 
-        <div class="l-contents__right">
-            <article class="l-contents__sidebar p-sidebar">
-                <div class="c-button__menu-close">
-                    <span class="c-button__menu-close__1"></span>
-                    <span class="c-button__menu-close__2"></span>
-                </div>
-                <ul class="p-sidebar__menu--main">
-                    <li><a href="#">バーガー</a>
-                        <ul class="p-sidebar__menu--sub">
-                            <li><a href="#">ハンバーガー</a></li>
-                            <li><a href="#">チーズバーガー</a></li>
-                            <li><a href="#">テリヤキバーガー</a></li>
-                            <li><a href="#">アボカドバーガー</a></li>
-                            <li><a href="#">フィッシュバーガー</a></li>
-                            <li><a href="#">ベーコンバーガー</a></li>
-                            <li><a href="#">チキンバーガー</a></li>
-                        </ul>
-                    </li>
+<?php get_sidebar(); ?>
 
-                    <li><a href="#">サイド</a>
-                        <ul class="p-sidebar__menu--sub">
-                            <li><a href="#">ポテト</a></li>
-                            <li><a href="#">サラダ</a></li>
-                            <li><a href="#">ナゲット</a></li>
-                            <li><a href="#">コーン</a></li>
-                        </ul>
-                    </li> 
-
-                    <li><a href="#">ドリンク</a>
-                        <ul class="p-sidebar__menu--sub">
-                            <li><a href="#">コーラ</a></li>
-                            <li><a href="#">ファンタ</a></li>
-                            <li><a href="#">オレンジ</a></li>
-                            <li><a href="#">アップル</a></li>
-                            <li><a href="#">紅茶（Ice/Hot）</a></li>
-                            <li><a href="#">コーヒー（Ice/Hot）</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </article>
-        </div>
-    </div>
-
-    <footer class="l-footer p-footer">
-        <div class="p-footer__menu">
-            <ul>
-                <li><a>ショップ情報</a></li>
-                <p>|</p>
-                <li><a>ヒストリー</a></li>
-            </ul>
-        </div>
-        <div class="p-footer__copy">
-            <p>Copyright: RaiseTech</p>
-        </div>
-    </footer>
-</body>
-</html>
+<?php get_footer(); ?>
