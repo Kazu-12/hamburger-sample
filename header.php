f2e5e7f709b2019ddc716cf594c0827c14ab8e32
Kazu <Kazu@MacBook-Air.local>
2021-05-30 21:50:13 +0900

    header.php記入

--- header.php
+++ header.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="<?php language_attributes(); ?>">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hamburger|ダミーサイト</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <script src="./js/jquery-3.6.0.min.js"></script>
-    <script src="./js/script.js"></script>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
