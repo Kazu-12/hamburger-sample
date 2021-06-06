5fd544c089eff6d55e18b19454372d3985aac405
Kazu <Kazu@MacBook-Air.local>
2021-06-06 17:06:43 +0900

    投稿表示設定

--- archive.php
+++ archive.php
@@ -1,73 +1,59 @@
 <?php get_header(); ?>
-        
-            <main class="l-main">
-                <div class="p-main-visual p-main-visual--archive">
-                    <div class="p-main-visual--archive--black-back">
-                        <h1>Menu:</h1>
-                        <p>チーズバーガー</p>
-                    </div>
-                </div>
-                <article class="p-article">
-                    <section class="p-main-description">
-                        <h2>小見出しが入ります</h2>
-                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                    </section>
-                    <div class="p-menu-list">
-                        <section class="c-menu-card">
-                            <img src="./img/menu-card.jpg" alt="チーズバーガー">
-                            <div class="c-menu-card__description">
-                                <h3>チーズバーガー</h3>
-                                <h4>小見出しが入ります</h4>
-                                <p class="c-menu-card__txt">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                                <p class="c-menu-card__detail-btn">
-                                    <a>詳しく見る</a>
-                                </p>    
-                            </div>
-                        </section>
 
-                        <section class="c-menu-card">
-                            <img src="./img/menu-card.jpg" alt="ダブルチーズバーガー">
-                            <div class="c-menu-card__description">
-                                <h3>ダブルチーズバーガー</h3>
-                                <h4>小見出しが入ります</h4>
-                                <p class="c-menu-card__txt">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                                <p class="c-menu-card__detail-btn">
-                                    <a>詳しく見る</a>
-                                </p>    
-                            </div>
-                        </section>
-
-                        <section class="c-menu-card">
-                            <img src="./img/menu-card.jpg" alt="スペシャルチーズバーガー">
-                            <div class="c-menu-card__description">
-                                <h3>スペシャルチーズバーガー</h3>
-                                <h4>小見出しが入ります</h4>
-                                <p class="c-menu-card__txt">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                                <p class="c-menu-card__detail-btn">
-                                    <a>詳しく見る</a>
-                                </p>
-                            </div>
-                        </section>
-                    </div> 
-                </article>
-                <div class="p-pagination">
-                    <ul>
-                        <li class="p-pagination__nav">Page 1/10</li>
-                        <li class="p-pagination__arrow"><a><img src="./img/pre.svg" alt="前へ"><span>&nbsp;前へ</span></a></li>
-                        <li class="p-pagination__num current-page"><a>1</a></li>
-                        <li class="p-pagination__num"><a>2</a></li>
-                        <li class="p-pagination__num"><a>3</a></li>
-                        <li class="p-pagination__num"><a>4</a></li>
-                        <li class="p-pagination__num"><a>5</a></li>
-                        <li class="p-pagination__num"><a>6</a></li>
-                        <li class="p-pagination__num"><a>7</a></li>
-                        <li class="p-pagination__num"><a>8</a></li>
-                        <li class="p-pagination__num"><a>9</a></li>
-                        <li class="p-pagination__arrow"><a><span>次へ&nbsp;</span><img src="./img/post.svg" alt="次へ"></a></li>
-                    </ul>
-                </div>
-            </main>
+<main class="l-main">
+    <div class="p-main-visual p-main-visual--archive">
+        <div class="p-main-visual--archive--black-back">
+            <h1>Menu:</h1>
+            <p><?php single_cat_title(); ?></p>
         </div>
+    </div>
+    <article class="p-article">
+        <section class="p-main-description">
+            <h2>小見出しが入ります</h2>
+            <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+        </section>
+        <div class="p-menu-list">
+
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()): ?>
+                    
+                    <?php the_post(); ?>
+                    <section class="c-menu-card">
+                        <img src="<?php the_post_thumbnail(); ?>">
+                        <div class="c-menu-card__description">
+                            <h3><?php the_title(); ?></h3>
+                            <h4><?php the_content(); ?></h4>
+                            <p class="c-menu-card__detail-btn">
+                                <a href="<?php the_permalink(); ?>">詳しく見る</a>
+                            </p>
+                        </div>
+                    </section>
+
+                <?php endwhile; ?>
+            <?php else :?>
+                <p>Menuが見つかりませんでした</p>
+            <?php endif; ?>
+        </div>
+    </article>
+    <div class="p-pagination">
+        <?php echo paginate_links(); ?>
+        <ul>
+            <li class="p-pagination__nav">Page 1/10</li>
+            <li class="p-pagination__arrow"><a><img src="<?php echo get_template_directory_uri() ?>/img/pre.svg" alt="前へ"><span>&nbsp;前へ</span></a></li>
+            <li class="p-pagination__num current-page"><a>1</a></li>
+            <li class="p-pagination__num"><a>2</a></li>
+            <li class="p-pagination__num"><a>3</a></li>
+            <li class="p-pagination__num"><a>4</a></li>
+            <li class="p-pagination__num"><a>5</a></li>
+            <li class="p-pagination__num"><a>6</a></li>
+            <li class="p-pagination__num"><a>7</a></li>
+            <li class="p-pagination__num"><a>8</a></li>
+            <li class="p-pagination__num"><a>9</a></li>
+            <li class="p-pagination__arrow"><a><span>次へ&nbsp;</span><img src="<?php echo get_template_directory_uri() ?>/img/post.svg" alt="次へ"></a></li>
+        </ul>
+    </div>
+</main>
+</div>
 
 <?php get_sidebar(); ?>
 
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
     <div class="l-contents__right">
-    
+
         <article class="l-contents__sidebar p-sidebar">
             <div class="c-button__menu-close">
                 <span class="c-button__menu-close__1"></span>
@@ -7,16 +7,16 @@
             </div>
             <ul class="p-sidebar__menu--main">
                 <?php
-                    if ( is_active_sidebar( 'menu_widget' ) ) :
-                    dynamic_sidebar( 'menu_widget' );
-                    else:
+                if (is_active_sidebar('menu_widget')) :
+                    dynamic_sidebar('menu_widget');
+                else :
                 ?>
-                <div class="widget">
-                    <h2>No Widget</h2>
-                    <p>ウィジットは設定されていません。</p>
-                </div>
+                    <div class="widget">
+                        <h2>No Widget</h2>
+                        <p>ウィジットは設定されていません。</p>
+                    </div>
                 <?php endif; ?>
             </ul>
         </article>
     </div> <!-- l-contents -->
-</div>
+    </div>
