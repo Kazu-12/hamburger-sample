8ce1359bd633b9e001176b043326952918a26695
Kazu <Kazu@MacBook-Air.local>
2021-06-11 01:42:16 +0900

    singleページWordPress化

--- footer.php
+++ footer.php
@@ -2,13 +2,6 @@
     <?php wp_nav_menu(array(
         'theme_location' => 'footer',
     )); ?>
-    <div class="p-footer__menu">
-        <ul>
-            <li><a>ショップ情報</a></li>
-            <p>|</p>
-            <li><a>ヒストリー</a></li>
-        </ul>
-    </div>
     <div class="p-footer__copy">
         <p>Copyright: RaiseTech</p>
     </div>
